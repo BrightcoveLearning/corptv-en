3f787f910699878da72c1ebbb0f05d027cc79dca
Robert Crooks <rcrooks@brightcove.com>
2022-03-21 13:27:13 -0400

    general-update

--- _site/assets/js/talla-init.js
+++ _site/assets/js/talla-init.js
@@ -1,7 +1,7 @@
-var appID = "00c26be3-4421-41e6-bb00-1dee68740f67";
+var appID = "";
 // token for anonymous profile
 var token =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
+'';
 // -----
   Talla.config = Talla.init(
     appID,
@@ -10,7 +10,7 @@ var token =
     Talla.OptionAnonymous(true),
     Talla.OptionThemeColor('rgb(1, 5, 14)'),
     Talla.OptionDisplayName('Search Brightcove CorpTV Docs'),
-    Talla.OptionKnowledgeGroups(['d4105468-75a8-4bbb-89ac-33df9e3b5173']),
+    Talla.OptionKnowledgeGroups(['']),
     Talla.OptionWideBeacon(true),
     Talla.OptionBeaconCTA("Have a question?"),
     Talla.OptionOpenIcon('https://support.brightcove.com/site-assets/images/site/search.png'),
