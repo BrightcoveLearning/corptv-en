eacf99161f3a04124756205aad7ddfda4b760aae
Matthew Boles <mboles@brightcove.com>
2022-04-20 13:56:22 -0400

    build site

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -1,6 +1,12 @@
 cd _includes
+echo 'Adding OTT plugins'
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins
+echo 'Adding Cloud Playout'
+git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
 cd ..
 cd assets/images
+echo 'Adding OTT plugins images'
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
+echo 'Adding Cloud Playout images'
+git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
 cd ..
