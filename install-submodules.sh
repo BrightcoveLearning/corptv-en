b1777bbea59cb9a8913a51048684ba7ace4dadf7
Matthew Boles <mboles@brightcove.com>
2022-04-26 14:33:45 -0400

    $COMMITMESSAGE

--- install-submodules.sh
+++ install-submodules.sh
@@ -3,10 +3,15 @@ echo 'Adding OTT plugins'
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins
 echo 'Adding Cloud Playout'
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
+echo 'Adding Layout'
+git submodule add https://github.com/BrightcoveLearning/s-layout
 cd ..
 cd assets/images
 echo 'Adding OTT plugins images'
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
 echo 'Adding Cloud Playout images'
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
+echo 'Adding Layout images'
+git submodule add https://github.com/BrightcoveLearning/s-layout-img
 cd ..
+git submodule update --init --recursive
