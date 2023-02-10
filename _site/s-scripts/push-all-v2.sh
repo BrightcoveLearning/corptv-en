8381af30f83d433c620cd429e95f13954b64c2a9
Robert <rcrooks@brightcove.com>
2023-02-10 16:52:08 -0500

    convert to new template

--- _site/s-scripts/push-all-v2.sh
+++ _site/s-scripts/push-all-v2.sh
@@ -1,6 +1,9 @@
 echo "Enter your ${bold}COMMIT MESSAGE${normal}:"
 read COMMITMESSAGE 
 cd ..
+git submodule update --remote
+git submodule foreach git checkout main
+git submodule foreach git pull origin main 
 git pull
 bundle update
 bundle exec jekyll build --trace --incremental
@@ -8,6 +11,9 @@ setopt localoptions rmstarsilent
 rm -f _site/feed.xml
 bold=`tput bold`
 normal=`tput sgr0`
+git submodule update --remote
+git submodule foreach git checkout main
+git submodule foreach git pull origin main 
 git pull
 git add .
 git commit -m $COMMITMESSAGE
